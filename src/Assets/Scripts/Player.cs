f28ff835a32f3038592947c5ac26d8157284628a
Aarron Hulswitt <aarron.hulswitt@gmail.com>
2024-02-29 06:45:46 -0500

    Added colliders to player and the individual floor pieces

--- src/Assets/Scripts/Player.cs
+++ src/Assets/Scripts/Player.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Rigidbody2D body;
     public float MoveSpeed = 5f;
     public float JumpSpeed = 2f;
     public float hitPoints = 100f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +24,8 @@ public class Player : MonoBehaviour
             var direction = new Vector3(horizontalMovement, 0, 0);
             gameObject.transform.Translate(direction);
         //} 
-        if(Input.GetKey(KeyCode.Space)){
-            var verticalMovement = Input.GetAxisRaw("Vertical") * JumpSpeed * Time.deltaTime;
-            var jumpDirection = new Vector3(verticalMovement, 0, 0);
-            gameObject.transform.Translate(jumpDirection);
+        if(Input.GetButtonDown("Jump")){
+            body.AddForce(Vector2.up * JumpSpeed, ForceMode2D.Impulse);
         }
     }
 }
